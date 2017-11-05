81d76f5397f142da93215fc22170c5cfd27761e0
Baek Gi Chan <11618nathan@gmail.com>
2017-11-05 21:46:05 +0900

    Update and rename complex number.py to basic/complex number.py

--- basic/complex number.py
+++ basic/complex number.py
@@ -1,9 +1,13 @@
 a = 2 + 3j
-print (a.real)
+
 #복소수의 실수 부분
-print (a.imag)
+print (a.real)
+
 #복소수의 허수 부분
-print (a.conjugate())
+print (a.imag)
+
 #복소수의 켤레복소수
-print (abs(a))
+print (a.conjugate())
+
 #절대값
+print (abs(a))
