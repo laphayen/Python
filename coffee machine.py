28fdcedfc9f40266b45a39fafde21c536971785b
Baek Gi Chan <11618nathan@gmail.com>
2017-11-07 00:03:55 +0900

    Update coffee machine.py

--- coffee machine.py
+++ coffee machine.py
@@ -1,39 +1,43 @@
-
 # 가격
 price = 300
 
 # 기본
-base = 10
+coffee = 10
 
-while (base > 0):
+while True:
     print('----- COFFEE MACHINE -----')
     money = int(input('돈을 넣어주세요: '))
-    
+
     # 2잔 이상
     if money >= 600:
         cup = int(input('몇 잔을 드릴까요?'))
-        money = money - ( price * cup )
-        base = (base - cup)
-        print(cup,'잔을 드립니다.')
-        print(money, '을 반환합니다.')
-    
+        money = money - (price * cup)
+        if money >= 0:
+            coffee = (coffee - cup)
+            print(cup, '잔을 드립니다.')
+            print(money, '을 반환합니다.')
+        else:
+            print('금액이 부족합니다.')
+            print('돈을 반환합니다.')
+
     # 1잔
     elif money > 300:
         money = money - price
-        base = (base - 1)
+        coffee = (coffee - 1)
         print('1잔을 드립니다.')
         print(money, '을 반환합니다.')
 
     # 1잔
     elif money == 300:
-        base = (base - 1)
+        coffee = (coffee - 1)
         print('1잔을 드립니다.')
-    
+
     # 반환
     else:
         print('돈이 부족합니다.')
         print('돈이 반환합니다.')
 
-    print(' ')
+    print('커피가 ', coffee, '잔 남았습니다.')
 
-print('All sold out. Come on tomorrow.')
+    if coffee == 0:
+        break
