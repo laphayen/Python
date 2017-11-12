e7a95175e961137d6be3a06745968d261299329e
Baek Gi Chan <11618nathan@gmail.com>
2017-11-12 22:54:09 +0900

    Create spiral.py

---
+++ spiral.py
@@ -0,0 +1,15 @@
+
+# 사각 나선
+import turtle as t
+
+def spiral(sp_len):
+    if sp_len <= 5:
+ 
+    t.forward(sp_len)
+    t.right(90)
+    spiral(sp_len - 5)
+
+t.speed(0)
+spiral(200)
+t.hideturtle()
+t.done()
