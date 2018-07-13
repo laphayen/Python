a13788e9cfc6049215a8839c7810c2b89405bf70
11618nathan <11618nathan@gmail.com>
2018-07-13 23:27:27 +0900

    Upload

---
+++ list.py
@@ -0,0 +1,18 @@
+# 리스트
+# 여러 개의 자료를 한 번에 지정
+# 여러 종류의 자료형을 넣을 수 있음.
+
+array = ["11618", "nathan", 11618, True, False]
+
+print(array)
+
+# 리스트 요소 출력 - 인덱스 0부터
+print(array[0])
+
+# 리스트 요소 변경
+array[0] = 7
+print(array[0])
+
+# 리스트 요소 접근
+print(array[-1])
+print(array[-2])
