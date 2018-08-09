2e04622bce27a787325a8fec77e1b80ce1f94440
11618nathan <11618nathan@gmail.com>
2018-08-09 20:35:15 +0900

    Upload

---
+++ Calculator/Calculator_function.py
@@ -0,0 +1,17 @@
+# 계산기 모듈 함수
+    
+# 팩토리얼
+def factorial(n):
+    return "factorial (!)"
+
+# 로마 숫자 변환
+def to_roman(n):
+    return "-> roman"
+
+# 2진수 변환
+def to_binary(n):
+    return "-> binary"
+
+# 10진수 변환
+def from_binary(n):
+    return "binary -> 10"
