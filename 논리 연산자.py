fcca7764baa4f9994890ea1ae8e20e198d2971ef
11618nathan <11618nathan@gmail.com>
2018-08-11 22:59:38 +0900

    Upload

---
+++ 논리 연산자.py
@@ -0,0 +1,12 @@
+# 논리 연산자
+
+# not Bool을 반대로 전환
+print(not True)
+print(not False)
+
+# and - 피연산자 두개가 모두 참일 때 True 출력, 나머지 경우 모두 False 출력
+num_one = 7
+num_two = 9
+print(num_one < 8 and num_two > 6)
+print(num_one < 7 and num_two > 5)
+print(num_one != 7 and num_two !=9)
