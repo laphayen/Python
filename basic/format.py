1b0a02b6dc822e144a3138975e15e4d47b709465
11618nathan <11618nathan@gmail.com>
2018-10-14 13:58:22 +0900

    Upload

---
+++ basic/format.py
@@ -0,0 +1,14 @@
+# format()
+# 문자열 변환 함수
+# {} 기호와 매개변수 조합
+
+output = "{}".format(11618)
+print(type(output))
+
+# 형식 조합
+output_num = "{}가지".format(119382)
+print(output_num)
+
+# 다중 조합
+output_com = "{} {} {}".format(1, 4, 5)
+print(output_com)
