845729995c092ff5d25a00d22577d7140cbfc1fc
11618nathan <11618nathan@gmail.com>
2020-09-20 17:06:42 +0900

    Upload isalpha.py

---
+++ isalpha.py
@@ -0,0 +1,20 @@
+
+print('codeomni')
+
+txt1 = 'abc'
+txt2 = 'ABC'
+txt3 = 'one'
+txt4 = 'one1'
+txt5 = '1'
+
+r1 = txt1.isalpha()
+r2 = txt2.isalpha()
+r3 = txt3.isalpha()
+r4 = txt4.isalpha()
+r5 = txt5.isalpha()
+
+print(r1)
+print(r2)
+print(r3)
+print(r4)
+print(r5)
