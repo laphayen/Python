09c6daa81876a74e1cfd1506bd5715f956b20006
11618nathan <11618nathan@gmail.com>
2020-09-20 17:09:44 +0900

    isdigit.py

---
+++ isdigit.py
@@ -0,0 +1,20 @@
+
+print('codeomni')
+
+txt1 = 'abc'
+txt2 = 'ABC'
+txt3 = 'one'
+txt4 = 'one1'
+txt5 = '1'
+
+r1 = txt1.isdigit()
+r2 = txt2.isdigit()
+r3 = txt3.isdigit()
+r4 = txt4.isdigit()
+r5 = txt5.isdigit()
+
+print(r1)
+print(r2)
+print(r3)
+print(r4)
+print(r5)
