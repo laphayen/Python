3662db1f995db17518e2f009bc87446db9057eba
Baek Gi Chan <11618nathan@gmail.com>
2021-10-17 21:51:20 +0900

    Update keywordlist.py

--- keywordlist.py
+++ keywordlist.py
@@ -1,2 +1,5 @@
 import keyword
 keyword.kwlist
+
+# 예상 출력
+# ['False', 'None', 'True', '__peg_parser__', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
