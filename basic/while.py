fa7944f3224350aa9e066a4a16e46ac334d3393a
11618nathan <11618nathan@gmail.com>
2021-11-01 23:05:40 +0900

    Update

--- basic/while.py
+++ basic/while.py
@@ -1,7 +1,20 @@
-### 반복문
 
-num = 0
+# 반복문
 
-while num < 100:
+# 특정 조건을 만족하는 경우 지속적으로 반복을 수행
+
+# while 조건:
+#   반복 실행 코드
+#   continue - 구문 처음으로 이동 후 반복
+#   break - while문 탈출
+
+num = 1
+
+while num < 5:
     print(num)
     num = num + 1
+    if num < 3:
+        continue
+    else:
+        break
+
