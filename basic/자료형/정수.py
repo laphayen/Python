73036ea939b40e4966ac49ffc280174c353fe3fb
11618nathan <11618nathan@gmail.com>
2021-11-07 23:22:49 +0900

    Update

---
+++ basic/자료형/정수.py
@@ -0,0 +1,15 @@
+
+# 정수형 자료
+# 파이썬의 정수형 상수의 최대, 최소값은 없다
+# 메모리가 허용하는 범위 내에서 사용.
+
+intdata = 10
+bindata = 0b10
+octdata = 0o10
+hexdata = 0x10
+hongdata = 10101010101010
+print(intdata)
+print(bindata)
+print(octdata)
+print(hexdata)
+print(hongdata)
---
+++ basic/제어문/while 무한반목문.py
@@ -0,0 +1,12 @@
+
+# while 무한반복문
+# Ture=1 False=0을 사용
+ 
+x = 1 
+sum = 0
+while 1:
+    sum = sum + 1
+    print(sum)
+    if sum < 100:
+        break
+    x =  x + 1
