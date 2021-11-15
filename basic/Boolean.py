26a146bc9a326532a22f92d9929a71cc67fdca34
11618nathan <11618nathan@gmail.com>
2021-11-15 22:16:35 +0900

    Update operator

--- basic/Boolean.py
+++ basic/Boolean.py
@@ -2,5 +2,9 @@
 # 기본적인 잘형으로 차모가 거짓을 나타내는 값.
 # 오직 True(참)과 False(거짓)만 값으로 가짐.
 
+# 1
 print(True)
+
+# 0
 print(False)
+
--- basic/연산자/논리 연산자.py
+++ basic/연산자/논리 연산자.py
@@ -1,8 +1,5 @@
 # 논리 연산자
 
-# not Bool을 반대로 전환
-print(not True)
-print(not False)
 
 # and - 피연산자 두개가 모두 참일 때 True 출력, 나머지 경우 모두 False 출력
 num_one = 7
@@ -10,3 +7,15 @@ num_two = 9
 print(num_one < 8 and num_two > 6)
 print(num_one < 7 and num_two > 5)
 print(num_one != 7 and num_two !=9)
+
+# or - 하나만이라도 참일 경우 True(1) 출력
+n1 = 1
+n2 = 0
+print(n1 or n1)
+print(n1 or n2)
+print(n2 or n2)
+
+# not Bool을 반대로 전환
+
+print(not True)
+print(not False)
