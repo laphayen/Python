5c4b55fb91ffc11ff0817119112dd918b648b7d7
11618nathan <11618nathan@gmail.com>
2021-11-17 22:54:33 +0900

    Upload progress

---
+++ utility/progress.py
@@ -0,0 +1,10 @@
+
+# 진행률
+
+from time import sleep
+for i in range(100):
+    msg = '\r진행률: %d%%' %(i+1)
+    print(''*len(msg), end='')
+    print(msg, end='')
+    print(msg, end='')
+    sleep(0.1)
