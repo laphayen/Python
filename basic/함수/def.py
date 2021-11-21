6eb8cf5bf794838e32948015e196d6133269d3ee
11618nathan <11618nathan@gmail.com>
2021-11-21 23:58:20 +0900

    Update  def

---
+++ basic/함수/def.py
@@ -0,0 +1,10 @@
+
+# 함수 - def의로 정의
+# 독립적 호출
+# 함수 생성
+
+def count(number):
+    n = 1
+    while n <= number:
+        print(n)
+        n = n + 1
--- function/def.py
+++
@@ -1,8 +0,0 @@
-# def
-# 함수 생성
-
-def count(number):
-    n = 1
-    while n <= number:
-        print(n)
-        n = n + 1
