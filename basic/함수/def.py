63d5f3a46c227176806d7ae85b808ae557c670e2
11618nathan <11618nathan@gmail.com>
2021-11-22 23:27:31 +0900

    Update def

--- basic/함수/def.py
+++ basic/함수/def.py
@@ -8,3 +8,27 @@ def count(number):
     while n <= number:
         print(n)
         n = n + 1
+
+# 리턴값O 인자O
+def name1(n1):
+    #코드
+    return 1
+
+# 리턴값O 인자X
+def name2():
+    # 코드
+    return 2
+
+# 리턴X 인자X
+def name3():
+    # 코드
+    return
+
+# 인자O 리턴O 호출
+n1 = name1(1)
+
+# 리턴X
+name3() 
+
+# 인자X 리턴X
+name3()
