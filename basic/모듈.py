5f4b3992df40d28afc5d5225e486f3e724d4131a
11618nathan <11618nathan@gmail.com>
2021-11-24 23:16:03 +0900

    Update module

--- basic/모듈.py
+++ basic/모듈.py
@@ -1,7 +1,9 @@
 
 # 모듈
 # 독립된 코드
-# import 키워드로 포함
+# import 모듈이름
+# import 패키지이름.모듈이름
+
 # 다른 파일 저장 후 import로 모듈 사용 가능 - 저장한 이름.py와 이름이 동일
 
 # import
@@ -10,3 +12,11 @@ print("10초 정지")
 time.sleep(10)
 print("10초 후 출력")
 
+# 계층 구조는 from 파일 사용
+from time import sleep
+sleep(10)
+print("print")
+
+ # 이름이 길 경우 as 별칭으로 사용
+ import 모듈이름 as 별칭
+ 
---
+++ 에러 처리/ImportError.py
@@ -0,0 +1,7 @@
+
+# 모듈 임포트 오류
+# ModuleNotFoundError: No module named 'modulename'
+
+
+import modulename
+
