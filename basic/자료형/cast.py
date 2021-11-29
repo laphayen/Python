8e55ef2d3efd699262aa1a9f76c42f5c37d564b3
11618nathan <11618nathan@gmail.com>
2021-11-29 14:43:41 +0900

    Update Boolean

---
+++ basic/자료형/cast.py
@@ -0,0 +1,21 @@
+
+# 캐스트
+# 자료형 변환
+
+# int() - 문자열을 int 자료형으로 변환
+output_int = int("17")
+print(output_int)
+print(type(output_int))
+
+# float() - 문자열을 float 자료형으로 변환
+output_float = float("1.1618")
+print(output_float)
+print(type(output_float))
+
+# str() - 자료형을 str 자료형으로 변환
+output_int = str(17)
+output_float = str(1.1618)
+print(output_int)
+print(output_float)
+print(type(output_int))
+print(type(output_float))
