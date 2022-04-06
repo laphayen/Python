ab25e72154301614ad85a5e7aed166dfcd53f745
Baek Gi Chan <11618nathan@gmail.com>
2022-04-06 21:21:25 +0900

    Upload

---
+++ basic/자료형/시퀀스 자료형/isalpha 문자열 문자 판단.py
@@ -0,0 +1,16 @@
+
+# isalpha
+# 문자열이 알파벳인지 검사
+ 
+
+txt1 = '1'
+txt2 = 'ㅎㅇ'
+txt3 = 16
+
+ret1 = txt1.isalpha()
+ret2 = txt2.isalpha()
+ret3 = txt3.isalpha()
+
+print(ret1)
+print(ret2)
+print(ret3)
---
+++ basic/자료형/시퀀스 자료형/isdigit 문자열 숫자 판단.py
@@ -0,0 +1,9 @@
+
+# isalpha
+# 문자열이 알파벳인지 검사
+ 
+txt1 = 11618
+
+ret1 = txt1.isdigit()
+
+print(ret1)
