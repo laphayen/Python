63ba6c263e663c0e2e66c8332a5fb5ef9771e393
Baek Gi Chan <11618nathan@gmail.com>
2022-04-08 19:54:09 +0900

    Upload max min

---
+++ basic/내장함수/max.py
@@ -0,0 +1,9 @@
+
+# max()
+# 최대값 구하기
+
+l = [8, 18, 1, 1.16]
+s = 'nathangithub'
+
+print(max(l))
+print(max(s))
---
+++ basic/내장함수/min.py
@@ -0,0 +1,10 @@
+
+# min()
+# 최대값 구하기
+
+l = [8, 18, 1, 1.16]
+s = 'nathangithub'
+
+print(min(l))
+print(min(s))
+ 
