9e5f022e2885487382924db03f2a491c6e6e8452
Baek Gi Chan <11618nathan@gmail.com>
2022-04-11 23:07:50 +0900

    Update find, join, split

--- basic/join.py
+++
@@ -1,7 +0,0 @@
-
-li = ['codeomni', 'tistory', 'com']
-
-dot ='.'
-
-result = dot.join(li)
-print(result)
---
+++ basic/내장함수/문자열 내장함수/join.py
@@ -0,0 +1,11 @@
+
+# join
+# 문자열을 특정 문자 또는 문자열로 결합
+# 리스트를 인자로 받아 문자열을 반환
+
+li = ['codeomni', 'tistory', 'com']
+
+dot ='.'
+
+result = dot.join(li)
+print(result)
