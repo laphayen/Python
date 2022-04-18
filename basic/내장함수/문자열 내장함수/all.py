bda7ebf38250d93bf42c7f75bb1beb67da7a329c
Baek Gi Chan <11618nathan@gmail.com>
2022-04-18 23:13:57 +0900

    Upload all.py, any.py

---
+++ basic/내장함수/문자열 내장함수/all.py
@@ -0,0 +1,11 @@
+
+# all
+# 리스트의 요소가 모두 참인지 확인
+
+l1 = [0, 1, 2, 3, 4]
+l2 = [True, True]
+l3 = ["", [], (), {}, None, False]
+
+print(all(l1))
+print(all(l2))
+print(all(l3))
---
+++ basic/내장함수/문자열 내장함수/any.py
@@ -0,0 +1,11 @@
+
+# any
+# 리스트의 요소가 모두 참인지 확인
+
+l1 = [0, 1, 2, 3, 4]
+l2 = [True, True]
+l3 = ["", [], (), {}, None, False]
+
+print(any(l1))
+print(any(l2))
+print(any(l3))
