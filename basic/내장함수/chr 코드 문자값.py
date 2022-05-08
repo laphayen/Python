ea53353b60a552445e8fc215d681fb2d363c91a4
Baek Gi Chan <11618nathan@gmail.com>
2022-05-08 23:54:21 +0900

    Update chr.py

--- basic/내장함수/chr 코드 문자값.py
+++ basic/내장함수/chr 코드 문자값.py
@@ -2,9 +2,16 @@
 # chr
 # 코드값에 해당하는 문자를 반환
 
+print('codeomni')
+
+# 변수에 문자를 입력
 val = input('문자 : ')
+
+# 자료형 변환
 val = int(val)
+
 try:
+    #chr() 함수 사용
     c = chr(val)
     print('코드값: %d, 문자: %s' %(val, c))
 except ValueError:
