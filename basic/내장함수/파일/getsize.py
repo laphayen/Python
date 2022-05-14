8171c24c7da8c0b966858d11b283a91300313569
Baek Gi Chan <11618nathan@gmail.com>
2022-05-14 23:58:30 +0900

    Upload getsize.py

---
+++ basic/내장함수/파일/getsize.py
@@ -0,0 +1,13 @@
+
+# 파일 크기 구하기
+# os.path 모듈의 getsize() 사용
+
+from os.path import getsize
+file = 'test.txt'
+
+# getsize()
+file_size = getsize(file)
+
+# 파일 크기 출력
+print('codeomni')
+print('file size: %d' %file_size)
