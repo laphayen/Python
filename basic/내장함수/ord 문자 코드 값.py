3d6e7f065ac7d55d7bdd048e79a1d07c4ef01431
Baek Gi Chan <11618nathan@gmail.com>
2022-05-18 23:59:23 +0900

    Update

--- basic/내장함수/ord 문자 코드 값.py
+++ basic/내장함수/ord 문자 코드 값.py
@@ -2,6 +2,7 @@
 # ord
 # 문자 코드값을 정수형으로 반환
 
+print('codeomni')
 c = 'A'
 r = ord(c)
-print(r)
+print('A =', r)
--- basic/내장함수/round 반올림.py
+++ basic/내장함수/round 반올림.py
@@ -1,10 +1,10 @@
 
 # round()
 # 반올림 함수 - 인자의 반올림을 반환
-# 정수, 실수
 
 r1 = round(16)
 r2 = round(-1.18) 
 
+print('codeoomni')
 print(r1)
 print(r2) 
